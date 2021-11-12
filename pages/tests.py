870a092e5370fcfb5803ba84b803ca8a0a5bc9ca
Chahat Kalsi <chahat.ck88@gmail.com>
2021-11-12 23:44:06 +0530

    all tests passed

--- pages/tests.py
+++ pages/tests.py
@@ -1,4 +1,5 @@
-from django.test import TestCase
+# pages tests
+
 from django.test import SimpleTestCase
 from django.urls import reverse
 # Create your tests here.
@@ -8,7 +9,6 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_status_code(self):
         response=self.client.get('/')
         self.assertEqual(response.status_code, 200)
-
 
     def test_homepage_url_name(self):
         response=self.client.get(reverse('home'))
