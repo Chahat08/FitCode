bfc158e8dcb361c8e7320862d6dcffbf844909ab
Chahat Kalsi <chahat.ck88@gmail.com>
2021-11-12 22:48:37 +0530

    basic boring templates made

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # mine
     'accounts',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
