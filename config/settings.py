84796598a88210f7b0472e0be105ac0abb54d3b2
Chahat Kalsi <chahat.ck88@gmail.com>
2021-11-13 00:25:54 +0530

    login redirect url changed

--- config/settings.py
+++ config/settings.py
@@ -148,3 +148,6 @@ STATICFILES_FINDERS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'home'
