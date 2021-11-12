0c5c757355b94d6ae87fb8cf9693c43b845d7e15
Chahat Kalsi <chahat.ck88@gmail.com>
2021-11-13 00:44:08 +0530

    tests added for auth, all passed

--- accounts/tests.py
+++ accounts/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse, resolve
 from .forms import CustomUserCreationForm
+from .views import SignupPageView
 
 # Create your tests here.
 
@@ -37,3 +38,32 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_superuser)
         self.assertTrue(admin_user.is_staff)
+
+class SignupPageTests(TestCase):
+
+    username='newuser'
+    email='newuser@email.com'
+
+
+    def setUp(self):
+        url=reverse('signup')
+        self.response=self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'registration/signup.html')
+        self.assertContains(self.response, 'Sign Up')
+
+    def test_signup_form(self):
+        new_user = get_user_model().objects.create_user(
+        self.username, self.email)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()
+        [0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()
+        [0].email, self.email)
+
+
+    def test_signup_view(self):
+        view=resolve('/accounts/signup/')
+        self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
