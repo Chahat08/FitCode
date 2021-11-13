58cf0f3a90ccb38a593f4003abf8e9125ead82b9
Chahat Kalsi <chahat.ck88@gmail.com>
2021-11-13 09:34:34 +0530

    allowed hosts changed for deployment later

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1', '.pythonanywhere.com]
 
 
 # Application definition
