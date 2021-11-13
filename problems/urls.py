22155535bcdea2b6989af76783b069b43b3f6253
Chahat Kalsi <chahat.ck88@gmail.com>
2021-11-13 10:48:48 +0530

    urls for prob detail view added

--- problems/urls.py
+++ problems/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProblemListView
+from .views import ProblemListView, ProblemDetailView
 
 urlpatterns=[
     path('', ProblemListView.as_view(), name='problem_list'),
+    path('<uuid:uuid>', ProblemDetailView.as_view(), name='problem_detail'),
     ]
