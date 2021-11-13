4d5c75a506c4883f5d1d0e7bdceb7b379561f033
Chahat Kalsi <chahat.ck88@gmail.com>
2021-11-13 12:23:40 +0530

    searching function also working!

--- problems/urls.py
+++ problems/urls.py
@@ -1,8 +1,10 @@
 # problems urls
 from django.urls import path
-from .views import ProblemListView, ProblemDetailView
+from .views import ProblemListView, ProblemDetailView, SearchResultsListView
 
 urlpatterns=[
     path('', ProblemListView.as_view(), name='problem_list'),
     path('<uuid:uuid>', ProblemDetailView, name='problem_detail'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
+
     ]
