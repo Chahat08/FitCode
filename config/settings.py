eaebc282b091dd824986e26658ef656af1d9b370
Chahat Kalsi <chahat.ck88@gmail.com>
2021-11-13 10:02:33 +0530

    fixtures for users added

--- config/settings.py
+++ config/settings.py
@@ -135,6 +135,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Fixture directory for auto-populating data
+FIXTURE_DIRS=[BASE_DIR/'fixtures']
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
