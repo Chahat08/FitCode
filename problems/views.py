b816fdf94cdb43d995e820afe97c986d705b1c82
Chahat Kalsi <chahat.ck88@gmail.com>
2021-11-13 10:52:54 +0530

    simple form for comment handling added

--- problems/views.py
+++ problems/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Problem
 from django.contrib.auth.mixins import LoginRequiredMixin
-
 # Create your views here.
 class ProblemListView(LoginRequiredMixin, ListView):
     model=Problem
