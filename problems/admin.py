84536df747dedc400c46b7efcbe989d8e3e379d7
Chahat Kalsi <chahat.ck88@gmail.com>
2021-11-13 10:56:47 +0530

    all models registered in admin

--- problems/admin.py
+++ problems/admin.py
@@ -1,3 +1,23 @@
+# problems admin
 from django.contrib import admin
+from .models import Problem, Comment, ProblemURL
 
 # Register your models here.
+class CommentInline(admin.TabularInline):
+    model=Comment
+    list_display=('comment', 'author', 'created')
+    list_filter=('created')
+    search_fields=('comment', 'author')
+
+class URLInline(admin.TabularInline):
+    model=ProblemURL
+    list_display=('url')
+
+class ProblemAdmin(admin.ModelAdmin):
+    inlines=[
+            CommentInline,
+            URLInline,
+        ]
+    list_display=('title', 'subject', 'topic')
+
+admin.site.register(Problem, ProblemAdmin);
--- problems/urls.py
+++ problems/urls.py
@@ -1,3 +1,4 @@
+# problems urls
 from django.urls import path
 from .views import ProblemListView, ProblemDetailView
 
