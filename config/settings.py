185644cdc7ae56e708bb27be2cb4ba67e6633b66
Chahat Kalsi <chahat.ck88@gmail.com>
2021-11-14 06:14:26 +0530

    static files collected, debug turned on for now

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DJANGO_DEBUG', default=False)
+DEBUG = env.bool('DJANGO_DEBUG', default=True)
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1', '.pythonanywhere.com', 'fitcode-tech.herokuapp.com/']
 
