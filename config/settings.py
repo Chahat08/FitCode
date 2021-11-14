70123fcca9844f6a89fd90ebc2ec40325b7d1a47
Chahat Kalsi <chahat.ck88@gmail.com>
2021-11-14 06:04:28 +0530

    secure_proxy_ssl_header updated

--- config/settings.py
+++ config/settings.py
@@ -97,6 +97,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': env.dj_db_url("DATABASE_URL",
     default="postgres://postgres@db/postgres")
@@ -147,6 +148,7 @@ SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
 SESSION_COOKIE_SECURE = env.bool("DJANGO_SESSION_COOKIE_SECURE", default=True)
 CSRF_COOKIE_SECURE = env.bool("DJANGO_CSRF_COOKIE_SECURE", default=True)
 
+SECURE_PROXY_SSL_HEADER=('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Fixture directory for auto-populating data
 FIXTURE_DIRS=[BASE_DIR/'fixtures']
