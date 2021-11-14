f9fc3723d75ab332b56fb9f3cac4ddc6a2a45576
Chahat Kalsi <chahat.ck88@gmail.com>
2021-11-14 05:50:47 +0530

    heroku.yml corrected

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1', '.pythonanywhere.com', 'fitcode-tech.herokuapp.com/']
 
 
 # Application definition
